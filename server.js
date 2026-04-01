74f7bbce063267a3c31bb7c06dc4ea0b2c614f8d
Tyler McKee <tylermckee@Tylers-MacBook-Pro.local>
2026-04-01 12:48:17 -0600

    update branding, simplify server, fetch from data.json

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Serve everything in /public statically
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Fallback to index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
